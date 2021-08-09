8977cb4ea0785cacf7e517b0ab804661f3fc481e
razamu15 <47922655+razamu15@users.noreply.github.com>
2021-08-09 10:59:37 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,29 +1,29 @@
 var loginConfig = {};
 loginConfig.PORT = 3000;
 loginConfig.TOKEN_TTL = 1200;
-loginConfig.TOKEN_SECRET = '7H15I5MaiI0G1n63(rEt';
+loginConfig.TOKEN_SECRET = '';
 loginConfig.SALT_ROUNDS = 5;
 
 var confluentConfig = {};
-confluentConfig.BROKERS = ['pkc-419q3.us-east4.gcp.confluent.cloud:9092'];
-confluentConfig.USERNAME = 'BVODY4UAEMXE626R';
-confluentConfig.PASSWORD = 'PIE2g0KllOgOOZXUXLP5+Oa+uMH0d+4lkqZfKKo1sgNo9I9WU+E8hO1rlnx7WrBI';
+confluentConfig.BROKERS = [''];
+confluentConfig.USERNAME = '';
+confluentConfig.PASSWORD = '';
 
 var alpacaConfig = {};
-alpacaConfig.KEY = 'PKDDZ1I5NLICTUYTR8PP';
-alpacaConfig.SECRET = 'ZajJwY6yqNTlxqechc5sZxy1CT1yuEdOwcdUHjNZ';
+alpacaConfig.KEY = '';
+alpacaConfig.SECRET = '';
 alpacaConfig.ENDPOINT = 'https://paper-api.alpaca.markets';
 
 var mongoConfig = {};
 mongoConfig.DATABASE = 'stonks';
 mongoConfig.USERNAME = 'cluster0';
-mongoConfig.PASSWORD = 'Blaovd1j7gYM0q6Q';
+mongoConfig.PASSWORD = '';
 mongoConfig.URI = `mongodb+srv://${mongoConfig.USERNAME}:${mongoConfig.PASSWORD}@cluster0.cqdv1.mongodb.net/${mongoConfig.DATABASE}?retryWrites=true&w=majority`;
 
 var redisConfig = {};
 redisConfig.REDIS_PORT = 19484;
 redisConfig.REDIS_HOST = 'redis-19484.c16.us-east-1-3.ec2.cloud.redislabs.com';
-redisConfig.REDIS_PASSWORD = 'j83wqsVpQ4mMFPet2PpNc49bONte4DDx';
+redisConfig.REDIS_PASSWORD = '';
 
 
 module.exports = {
